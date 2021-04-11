9b1144a4e329d365287fd0368d3d69eda80c27b4
mr-emeka <speak2c.emeka@gmail.com>
2021-04-11 18:06:31 +0100

    setup and host web app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,117 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
+import { baseURL, socketURL, formatData } from './helpers'
+import Dashboard from './component/views/dashboard'
 
 function App() {
+  const [currencies, setCurrencies] = useState<any[]>([]);
+  const [pair, setPair] = useState<string>("");
+  const [price, setPrice] = useState<string>("0.00");
+  const [pastData, setPastData] = useState<any>({});
+
+  const ws = useRef<any>(null);
+  let first = useRef<boolean>(false);
+
+  useEffect(() => {
+    //connect to websocket API
+    ws.current = new WebSocket(socketURL);
+    //inside useEffect we need to make API with async function  
+    const apiCall = async () => {
+      let pairs: any[] = await fetch(baseURL + "/products")
+        .then((res) => res.json())
+        .then((data) => data);
+
+      //coinbase returns over 120 currencies, this will filter to only USD based pairs
+      let filtered = pairs.filter((pair) => pair.quote_currency === "USD")
+
+      //sort filtered currency pairs alphabetically
+      filtered = filtered.sort((a, b) => {
+        return (a.base_currency < b.base_currency) ?
+          -1 :
+          (a.base_currency > b.base_currency) ?
+            1 : 0;
+      });
+
+      setCurrencies(filtered);
+
+      first.current = true;
+    };
+
+    //call async function
+    apiCall()
+  }, [])
+
+
+  useEffect(() => {
+    //prevents this hook from running on initial render
+    if (!first.current) {
+      return;
+    }
+
+    let msg = {
+      type: "subscribe",
+      product_ids: [pair],
+      channels: ["ticker"]
+    };
+
+    let jsonMsg = JSON.stringify(msg);
+    ws.current.send(jsonMsg);
+
+    let historicalDataURL = `${baseURL}/products/${pair}/candles?granularity=86400`;
+
+    const fetchHistoricalData = async () => {
+      let dataArr: never[] = [];
+      await fetch(historicalDataURL)
+        .then((res) => res.json())
+        .then((data) => (dataArr = data));
+
+      //helper function to format data that will be implemented later
+      let formattedData = formatData(dataArr);
+      setPastData(formattedData);
+    };
+    //run async function to get historical data
+    fetchHistoricalData();
+    //need to update event listener for the websocket object so that it is listening for the newly updated currency pair
+    ws.current.onmessage = (e: { data: string; }) => {
+      let data = JSON.parse(e.data);
+      if (data.type !== "ticker") {
+        return;
+      }
+      //every time we receive an even from the websocket for our currency pair, update the price in state
+      if (data.product_id === pair) {
+        setPrice(data.price);
+      }
+    };
+    //dependency array is passed pair state, will run on any pair state change
+  }, [pair]);
+
+  const handleSelect = (e: { target: { value: React.SetStateAction<string>; }; }) => {
+    let unsubMsg = {
+      type: "unsubscribe",
+      product_ids: [pair],
+      channels: ["ticker"]
+    };
+    let unsub = JSON.stringify(unsubMsg);
+
+    ws.current.send(unsub);
+
+    setPair(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {
+        <select name="currency" value={pair} onChange={handleSelect}>
+          {currencies.map((cur, idx) => {
+            return (
+              <option key={idx} value={cur.id}>
+                {cur.display_name}
+              </option>
+            );
+          })}
+        </select>
+      }
+      <Dashboard price={price} data={pastData} />
     </div>
   );
 }
---
+++ src/helpers/index.ts
@@ -0,0 +1,50 @@
+const baseURL = "https://api.pro.coinbase.com";
+const socketURL = "wss://ws-feed.pro.coinbase.com";
+
+const formatData = (data: any[]) => {
+  let finalData: any = {
+    labels: [],
+    datasets: [
+      {
+        label: "Price",
+        data: [],
+        backgroundColor: "rgb(255, 99, 132, 0.8)",
+        borderColor: "rgba(255, 99, 132, 0.2)",
+        fill: false,
+      },
+    ],
+  };
+
+  //convert dates from timestamp to mm/dd/yy format
+  let dates = data.map((val: any[]) => {
+    const ts = val[0];
+    let date = new Date(ts * 1000);
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    let final = `${month}-${day}-${year}`;
+    return final;
+  });
+
+  //coinbase API returns multiple price values, we want the ending price for that day
+  let priceArr = data.map((val: any[]) => {
+    return val[4];
+  });
+
+  //reverse price array so it is in chronological order
+  priceArr.reverse();
+
+  //do same for dates
+  dates.reverse();
+
+  //set data labels as the date array for ChartJS
+  finalData.labels = dates;
+
+  //price array will be used as dataset for ChartJS
+  finalData.datasets[0].data = priceArr;
+
+  return finalData;
+};
+
+export { baseURL, socketURL, formatData };
